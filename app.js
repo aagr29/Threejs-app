d19e4f6f5b69d1750a7ae826bc031ec31112b410
aagr29 <63660748+aagr29@users.noreply.github.com>
2022-09-02 10:40:08 +0930

    clean up app.js

--- app.js
+++ app.js
@@ -52,31 +52,21 @@ function projectionTransform(secondProjection, x, y) {
     let firstProjection = "EPSG:4326"
     return proj4(firstProjection, secondProjection, [x, y]);
 }
-var y_pole_1=-28.543944504006674
-var x_pole_1=115.51127211328847
-var z_pole_1=294.022
+var y_pole_1=-28.543944504006674 // origin y coordinate ----------- this is our reference origin 
+var x_pole_1=115.51127211328847 // origin x coordinate ----------- this is our reference origin 
 var l1=projectionTransform("EPSG:28350",x_pole_1,y_pole_1)
 var ref_x=l1[0]
 var ref_y=l1[1]
 console.log(l1)
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+// Poles data wrangling
 var data = fs.readFileSync('./Poles.csv')
     .toString() // convert Buffer to string
     .split('\n') // split string to lines
     .map(e => e.trim()) // remove white spaces for each line
     .map(e => e.split(',').map(e => e.trim())); // split each line to array
-//  console.log(data[1][6]);
-//  console.log(JSON.stringify(data, '', 2)); // as json
-// let regExpLiteral1 = /(.*?)\)/
-// var coordinates=data[1][6].match(regExpLiteral1)
-// console.log(coordinates[1])
-// const myArray = coordinates[1].split(" ")
-// console.log(myArray)
-// let x_coordinate=parseFloat(myArray[0])
-// let y_coordinate=parseFloat(myArray[1])
-// let z_coordinate=parseFloat(myArray[2])
-// console.log(x_coordinate)
-// console.log(y_coordinate)
-// console.log(z_coordinate)
+
 var array_x_coordinate = [];
 var array_y_coordinate = [];
 var array_z_coordinate = [];
@@ -94,12 +84,7 @@ for (let step = 1; step < data.length-1; step++) {
     array_y_coordinate[i]=y_coordinate
     array_z_coordinate[i]=z_coordinate
   }
-
-// console.log(array_x_coordinate)
-// console.log(array_y_coordinate)
-// console.log(array_z_coordinate)
-// var l2=projectionTransform("EPSG:28350",x_pole_2,y_pole_2)
-
+//conert lat long to meters
 var x_meters=[]
 var y_meters=[]
 var z_meters=[]
@@ -113,7 +98,7 @@ for (let step = 0; step < array_x_coordinate.length; step++) {
 console.log(x_meters)
 console.log(y_meters)
 console.log(z_meters)
-
+// send data to client in json
 app.get('/get_coordinates', function(req, res) {
   
      
@@ -123,18 +108,13 @@ app.get('/get_coordinates', function(req, res) {
  
   })
 
-
-
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Conductors data wrangling
 var data_conductors = fs.readFileSync('./Conductors.csv')
     .toString() // convert Buffer to string
     .split('\n') // split string to lines
     .map(e => e.trim()) // remove white spaces for each line
     .map(e => e.split(',').map(e => e.trim())); // split each line to array
-
-console.log(data_conductors)
-
-
 var xc1= [];
 var yc1= [];
 var zc1= [];
@@ -252,11 +232,7 @@ for (let step = 1; step < data_conductors.length-1; step++) {
     zc11[i]=z_coordinate11
   }
 
-console.log(xc11)
-console.log(zc11)
-console.log(xc11.length)
-console.log(zc11.length)
-
+// convert lat long to meters
 for (let step = 0; step < array_x_coordinate.length; step++) {
     let coordinates=projectionTransform("EPSG:28350",array_x_coordinate[step],array_y_coordinate[step])
     x_meters[step]=coordinates[0]-ref_x
@@ -346,7 +322,7 @@ for (let step = 0; step < array_x_coordinate.length; step++) {
   }
 
 
-
+// send data to client in json
 app.get('/get_coordinates_conductors', function(req, res) {
   
      
